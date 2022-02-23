34df614168afad7c04282521fb7f065f9c68883c
mohamed hosni <mohamed.hosni3202@gmail.com>
2022-02-23 16:46:21 +0200

    first commit

---
+++ resources/lang/en/genres.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'genres' => 'Categories',
+    'genre' => 'Category',
+    'name' => 'Name',
+    'movies_count' => 'Tours count',
+    'related_tours' => 'Related Tours',
+];
